98ffd26c21081a6a1c4af02697872593658c8c68
pengjinghuan <pengjinghuan@sinohealth.cn>
2021-07-12 21:41:54 +0800

    test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.status(203).send({ id: 12221, name: "avocado" });
+});
+
+router.post("/save", function (req, res) {
+  console.log("收到请求体：", req.body);
+  res.status(201).send("保存成功");
 });
 
 module.exports = router;
